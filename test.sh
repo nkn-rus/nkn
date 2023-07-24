4f859a5e68bcedfd1057abaa45c63157c980d4b9
nkn-rus <135843984+nkn-rus@users.noreply.github.com>
2023-07-24 22:53:47 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-IP=$(hostname -I)
-curl -X POST -d "{\"ip\": \"$IP\", \"exists\": false}" http://5.180.183.19:9999
+export IP=$(hostname -I)
+curl -X POST -d "{\"ip\": \"$IP\"}" 75.119.144.234:9999/node/make
